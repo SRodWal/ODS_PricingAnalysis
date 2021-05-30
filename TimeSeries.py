d34913cbe840ab59be6fe48792238a15bb486e0e
Samuel Rod-Wal <83095093+SRodWal@users.noreply.github.com>
2021-05-30 10:08:45 -0500

    Update TimeSeries.py

--- TimeSeries.py
+++ TimeSeries.py
@@ -44,7 +44,8 @@ dfs["Time"] = timevec
 dfs = dfs.set_index("Time")    
 ###### Definicion de plantas
 #site = dfs.columns[50] # Guaymas
-site = dfs.columns[64] #Guaimaca
+#site = dfs.columns[64] #Guaimaca
+site = dfs.columns[87] #Caracol Knits
 
 ###### Series de tiempo
 dfs["Año"] = dfs.index.year
@@ -55,14 +56,14 @@ dfs["Por hora"] = dfs.index.hour
 
 # Graficas
 plt.figure(num = 1, figsize = (12, 6))
-dfs[dfs.columns[50]].plot(marker='.', alpha=0.2, figsize=(12, 6))
+dfs[site].plot(marker='.', alpha=0.2, figsize=(12, 6))
 plt.ylabel("Precios $/MWh")
 plt.xlabel("Tiempo")
 plt.title("Costos Marginales - "+site)
 plt.show()
 
 plt.figure(num = 2, figsize = (10, 6))
-sb.boxplot(data = dfs, x = 'Año', y = dfs.columns[50])
+sb.boxplot(data = dfs, x = 'Año', y = site)
 plt.title("Costos Marignales Promedio Anuales")
 plt.show()
 
